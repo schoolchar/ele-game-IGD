8f7181c8134a1dae1597b5141f0c9e61ed4b4589
SadieRaghunand1 <sadielrowl@outlook.com>
2025-03-06 12:30:00 -0500

    Add comments and death check to playerhealth

--- Assets/Scripts/PlayerControls/PlayerHealth.cs
+++ Assets/Scripts/PlayerControls/PlayerHealth.cs
@@ -12,12 +12,34 @@ public class PlayerHealth : MonoBehaviour
         LoseHealth(other);
     }
 
-
+    /// <summary>
+    /// Decrease player health by 1 when it collides with an enemy
+    /// </summary>
     void LoseHealth(Collider _other)
     {
+        //Check for enemy
         if(_other.gameObject.tag == "Enemy")
         {
+            //Decrement health
             health--;
+
+            //Check if player is at 0 health
+            CheckForDeath();
         }
-    }
+    } //END LoseHealth()
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void CheckForDeath()
+    {
+        //Check if player is at 0 health
+        if(health <= 0)
+        {
+            //Run death code
+            Debug.Log("Player dies");
+        }
+        
+    } //END CheckForDeath()
+    
 }
