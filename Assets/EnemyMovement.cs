8658f6a8dd2badb01b3be63ba6e42bcedcf033dc
LilyW3865 <li733414@ucf.edu>
2025-03-09 21:42:05 -0400

    Enemy movement updated

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -2,59 +2,15 @@ using UnityEngine;
 using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
-    public Transform patrolRoute; // Parent containing waypoints
     public Transform player; // Player reference
-    private NavMeshAgent agent;
-    private Transform[] locations;
-    private int currentLocation = 0;
-    public bool chasingPlayer = false;
-
+    public int Movespeed;
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-        InitializePatrolRoute();
-        MoveToNextPatrolLocation();
+        Movespeed = 5;
+        player = player.transform;
     }
     void Update()
     {
-        if (!chasingPlayer && !agent.pathPending && agent.remainingDistance < 0.2f)
-        {
-            MoveToNextPatrolLocation();
-        }
-    }
-    public void MoveToNextPatrolLocation()
-    {
-        if (locations.Length == 0) return;
-        agent.SetDestination(locations[currentLocation].position);
-        currentLocation = (currentLocation + 1) % locations.Length;
-    }
-    void InitializePatrolRoute()
-    {
-        locations = new Transform[patrolRoute.childCount];
-        for (int i = 0; i < patrolRoute.childCount; i++)
-        {
-            locations[i] = patrolRoute.GetChild(i);
-        }
-    }
-    
-    // Detect when player enters enemy's range
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.name == "Player")
-        {
-            Debug.Log("Player detected!");
-            chasingPlayer = true;
-            agent.SetDestination(player.position);
-        }
-    }
-    // Detect when player leaves enemy's range
-    void OnTriggerExit(Collider other)
-    {
-        if (other.name == "Player")
-        {
-            Debug.Log("Player out of range - resume patrol.");
-            chasingPlayer = false;
-            MoveToNextPatrolLocation();
-        }
+        transform.position = Vector3.MoveTowards(transform.position, player.position, Movespeed * Time.deltaTime);
     }
 }
