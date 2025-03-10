935dd013db8de607a5cadd2454a023581fa23843
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-03-10 12:29:25 -0400

    SpawnSetUp

--- Assets/EnemySpawn.cs
+++ Assets/EnemySpawn.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
     public GameObject[] enemies;
+    public GameObject player;
 
-    public Vector3 spawnValues;
+    public float minSpawnDistance = 5f;
+    public float maxSpawnDistance = 10f;
     public float spawnWait;
     public float spawnMostWait;
     public float spawnLeastWait;
@@ -29,13 +32,17 @@ public class EnemySpawn : MonoBehaviour
 
     IEnumerator waitSpawner()
     {
+        Vector3 playerPosition = player.transform.position;
+
         yield return new WaitForSeconds(startWait);
 
         while (!stop)
         {
             randomEnemy = Random.Range(0, 1);
-            Vector3 spawnPosition = new Vector3(1, 1, 0);
-            Instantiate(enemies[randomEnemy], spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+            float angle = Random.Range(0f, 360f) * Mathf.Deg2Rad;
+            float distance = Random.Range(minSpawnDistance, maxSpawnDistance);
+            Vector3 spawnPosition = playerPosition + new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * distance;
+            Instantiate(enemies[randomEnemy], spawnPosition, gameObject.transform.rotation);
             yield return new WaitForSeconds(spawnWait);
         }
     }
