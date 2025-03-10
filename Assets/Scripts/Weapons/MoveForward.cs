f63780000dd2ada03cbeeb26636d841a0599d6e3
SadieRaghunand1 <sadielrowl@outlook.com>
2025-03-10 18:07:02 -0400

    Player shoots at 3 second intervals, destroys enemy

---
+++ Assets/Scripts/Weapons/MoveForward.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public int speed = 5;
+
+    private void Start()
+    {
+        StartCoroutine(BulletLifetime());
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        HitEnemy(collision);
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+
+    void HitEnemy(Collision _collision)
+    {
+        if(_collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Enemy hit");
+            Destroy(_collision.gameObject);
+            DestroyBullet();
+        }
+    }
+
+    void DestroyBullet()
+    {
+        Destroy(this.gameObject);
+    }
+
+    IEnumerator BulletLifetime()
+    {
+        yield return new WaitForSeconds(5);
+        DestroyBullet();
+    }
+}
+
