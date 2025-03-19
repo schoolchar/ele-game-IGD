757d66903485a16e84d7e4d5ec88cf095eaa8df4
SadieRaghunand1 <sadielrowl@outlook.com>
2025-03-19 12:17:15 -0400

    Mostly done with elephant functionality
    Need to add comments, test, and add animations

--- Assets/Scripts/PlayerControls/UnlockableCharacters/Elephant.cs
+++ Assets/Scripts/PlayerControls/UnlockableCharacters/Elephant.cs
@@ -4,8 +4,60 @@ using UnityEngine;
 
 public class Elephant : MonoBehaviour, ICharacterActions
 {
+    //Elephant has buckets of water on its sides, sprays water (long range attack, slowly expands in range);
+
+    [SerializeField] private GameObject[] waterPlanes;
+    private float duration = 3;
+    private float timeBWAttack = 4;
+
+
+    void Start()
+    {
+        StartCoroutine(StartWaterSpout());
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            Attack(other.gameObject.GetComponent<EnemyHealth>()); 
+        }
+    }
+
+
     public void Attack(EnemyHealth _enemy)
     {
+        _enemy.TakeDamage(0.5f); //Placeholder value
+    }
 
+    private IEnumerator DelayWaterSpread(int _nextWaterIndx)
+    {
+        yield return new WaitForSeconds(0.5f);
+        waterPlanes[_nextWaterIndx].SetActive(true);
+
+        if (waterPlanes[_nextWaterIndx + 1] != null)
+        {
+            StartCoroutine(DelayWaterSpread(_nextWaterIndx + 1));
+        }
+    }
+
+    private IEnumerator StartWaterSpout()
+    {
+        yield return new WaitForSeconds(timeBWAttack);
+        waterPlanes[0].SetActive(true);
+        StartCoroutine(EndWaterSpout());
+        StartCoroutine(DelayWaterSpread(1));
+    }
+
+    private IEnumerator EndWaterSpout()
+    {
+        yield return new WaitForSeconds(duration);
+
+        for(int i = 0; i < waterPlanes.Length; i++)
+        {
+            waterPlanes[i].SetActive(false);
+        }
+        StopAllCoroutines();
+        StartCoroutine(StartWaterSpout());
     }
 }
