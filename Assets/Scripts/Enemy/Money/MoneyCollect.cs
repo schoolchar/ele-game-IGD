6bc993810cbbc308174ead208332906bd013181c
SadieRaghunand1 <sadielrowl@outlook.com>
2025-04-03 13:46:58 -0400

    Finish coin collection code

--- Assets/Scripts/Enemy/Money/MoneyCollect.cs
+++ Assets/Scripts/Enemy/Money/MoneyCollect.cs
@@ -6,9 +6,31 @@ public class MoneyCollect : MonoBehaviour
 {
     //Attach to coins, make collider much larger than coin to make it easier for player to get the coins
 
+    private int secondsBeforeDespawn = 5;
+
+    private void Start()
+    {
+        StartCoroutine(Despawn());
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        AddMoney(other);
+    }
+
+    void AddMoney(Collider _other)
+    {
+        if(_other.gameObject.GetComponent<PlayerHealth>() != null)
+        {
+            PlayerHealth _player = _other.gameObject.GetComponent<PlayerHealth>();
+            _player.money++;
+            Destroy(this.gameObject);
+        }
+    }
+
+    IEnumerator Despawn()
+    {
+        yield return new WaitForSeconds(secondsBeforeDespawn);
+        Destroy(this.gameObject);
     }
 }
