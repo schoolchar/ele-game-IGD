5f2656f769f2504ce3b0898fd12b1d93bd6822d1
corinnedaigle3 <corinnedaigle9@gmail.com>
2025-04-10 11:47:06 -0400

    Spawner Will increse over time, and harder enemies will spawn over time

--- Assets/Scripts/Enemy/EnemySpawn.cs
+++ Assets/Scripts/Enemy/EnemySpawn.cs
@@ -5,22 +5,30 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    public GameObject[] enemies;
+    //public GameObject[] enemies;
     public GameObject player;
+    public List<GameObject> spawns;
+
+    public GameObject rat1;
+    public GameObject rat2;
+    public GameObject clown;
+    public GameObject bear;
 
     public float spawnDistanceMin = 10f;
     public float spawnDistanceMax = 14f;
     public float spawnWait;
-    public float spawnMostWait = 8f;
-    public float spawnLeastWait = 4f;
+    public float spawnMostWait = 12f;
+    public float spawnLeastWait = 6f;
     public int startWait;
     public bool stop;
     public float timeToIncrease = 4f;
+    public float timeToAddBear = 60f;
+    public float timeToAddClown = 90f;
     public float increaseAmount = 0.5f;
 
     private float timer;
-    float timeUntilSpawnRateIncrease = 3f;
-    float spawnRateCountdown;
+    private float addBearTimer;
+    private float addClownTimer;
 
     private int randomEnemy;
 
@@ -29,13 +37,40 @@ public class EnemySpawn : MonoBehaviour
     {
         player = GameObject.FindWithTag("Player");
         StartCoroutine(waitSpawner());
+
     }
 
     // Update is called once per frame
     void Update()
     {
         spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
-        spawnRateCountdown -= Time.deltaTime;
+        timer += Time.deltaTime;
+        addBearTimer += Time.deltaTime;
+        addClownTimer += Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        if (timer >= timeToIncrease)
+        {
+            if (spawnMostWait > 4f) // Prevent negative or very small intervals
+            {
+                spawnMostWait -= increaseAmount;
+            }
+            timer = 0f;
+        }
+
+        if (addBearTimer >= timeToAddBear)
+        {
+            spawns.Add(bear);
+            addBearTimer = 0f;
+        }
+
+        if (addClownTimer >= timeToAddClown)
+        {
+            spawns.Add(clown);
+            addClownTimer = 0f;
+        }
     }
 
     IEnumerator waitSpawner()
@@ -45,22 +80,11 @@ public class EnemySpawn : MonoBehaviour
 
         while (!stop)
         {
-            timer += Time.deltaTime;
-
-            if (timer >= timeToIncrease)
-            {
-                if (spawnMostWait > 2.1f) // Prevent negative or very small intervals
-                {
-                    spawnMostWait -= increaseAmount;
-                }
-                timer = 0f;
-            }
-
-            randomEnemy = Random.Range(0, 1);
+            randomEnemy = Random.Range(0, 8);
             float angle = Random.Range(0f, 360f) * Mathf.Deg2Rad;
             float distance = Random.Range(spawnDistanceMin, spawnDistanceMax);
             Vector3 spawnPosition = playerPosition + new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * distance;
-            Instantiate(enemies[randomEnemy], spawnPosition, player.transform.rotation);
+            Instantiate(spawns[randomEnemy], spawnPosition, player.transform.rotation);
             yield return new WaitForSeconds(spawnWait);
         }
     }
