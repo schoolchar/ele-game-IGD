bbb31503e7c9a4aa3dc50e61a7e872dbabdaa2f9
SadieRaghunand1 <sadielrowl@outlook.com>
2025-04-12 18:46:28 -0400

    Add system exception and try catch for iif player health is less than 0

--- Assets/Scripts/PlayerControls/PlayerHealth.cs
+++ Assets/Scripts/PlayerControls/PlayerHealth.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 using System.IO;
+using Unity.VisualScripting;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -58,6 +59,8 @@ public class PlayerHealth : MonoBehaviour
         }
 
         health = maxHealth;
+
+      
         //Change later to not use tags
         hpText = GameObject.FindGameObjectWithTag("HPText").GetComponent<TextMeshProUGUI>();
         xpText = GameObject.FindGameObjectWithTag("XPText").GetComponent<TextMeshProUGUI>();
@@ -84,10 +87,32 @@ public class PlayerHealth : MonoBehaviour
         {
             //Decrement health
             health--;
-            hpText.text = "Health = " + health.ToString();
+            int _tempHealth = health;
 
-            //Check if player is at 0 health
-            CheckForDeath();
+
+            //Check if player is at 0 or less health
+            try
+            {
+                //Try to determine death state
+                CheckForDeath();
+                Debug.Log("Player health is greater than zero, can be passed");
+            }
+            catch(System.ArgumentException e)
+            {
+                //If health is less than 0
+                health = 0;
+                CheckForDeath();
+                Debug.Log("Player health reset to 0 " + e);
+            }
+            finally
+            {
+                //Health has been checked
+                Debug.Log("Player health checked for death");
+            }
+
+
+            //Display current health, this way it never shows less than 0 health
+            hpText.text = "Health = " + health.ToString();
         }
     } //END LoseHealth()
 
@@ -96,6 +121,11 @@ public class PlayerHealth : MonoBehaviour
     /// </summary>
     void CheckForDeath()
     {
+       if(health < 0)
+        {
+            throw new System.Exception("Player health cannot be less than 0");
+        }
+
         //Check if player is at 0 health
         if (health <= 0)
         {
