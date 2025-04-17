468ea9839569929c3e57596b43684d9843e68a06
SadieRaghunand1 <sadielrowl@outlook.com>
2025-04-17 11:33:19 -0400

    Start dialogue system
    Need to write the lines on doc, read from doc, and put in text scrolling

---
+++ Assets/Scripts/Menus/DialogueManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private Image textBox;
+
+    [SerializeField] private string[] lines; //Change to a stack? make it only fill back up when data is cleared?
+    [SerializeField] private string currentLine;
+
+    //Test
+    public Stack<string> dialogueLines = new Stack<string>(3);
+
+
+    private void Start()
+    {
+        dialogueLines.Push("Third line");
+        dialogueLines.Push("Second line");
+        dialogueLines.Push("First line"); //Read from a file, this is testing
+
+
+        ShowNextLine();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ShowNextLine();
+        }
+    }
+
+    public void ShowNextLine()
+    {
+        if(dialogueLines.Count == 0)
+        {
+            Debug.Log("Stack null");
+            dialogueText.enabled = false;
+            textBox.enabled = false;
+            return;
+        }
+
+        currentLine = dialogueLines.Pop();
+
+        dialogueText.text = currentLine;
+
+        
+    } 
+
+    
+}
